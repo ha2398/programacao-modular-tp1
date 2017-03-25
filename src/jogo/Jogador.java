4054632b7b02b71ab6a315bacaddfe2438f73a00
Jessica <jessica@Luizs-MacBook-Air.local>
2017-03-25 11:19:00 -0300

    Exemplo de teste com o JUnit.

--- src/jogo/Jogador.java
+++ src/jogo/Jogador.java
@@ -1,5 +1,27 @@
 package jogo;
 
 public class Jogador {
+	private String name;
+	private double balance;
 
+	public Jogador(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public void decreaseBalance(double amount) {
+		this.balance -= amount;
+	}
+
+	public void increaseBalance(double amount) {
+		this.balance += amount;
+	}
 }
