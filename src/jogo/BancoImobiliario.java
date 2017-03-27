64414fbc1d7963b97a2255926d03d5ab512a4c7d
Hugo Sousa <hsousa@gmx.com>
2017-03-26 22:48:22 -0300

    [Tabuleiro / Entrada] Método constroiTabuleiro, classe PosicaoTabuleiro, arquivo de entrada -Método constroiTabuleiro implementado. -Classe PosicaoTabuleiro com pequenas alterações, implementado construtor. -Arquivo de entrada exemplo adicionado (tabuleiro.txt para construção do tabuleiro de jogo).

--- src/jogo/BancoImobiliario.java
+++ src/jogo/BancoImobiliario.java
@@ -1,9 +1,17 @@
 package jogo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BancoImobiliario {
-	private static ArrayList<PosicaoTabuleiro> tabuleiro;
+	private static final String nomeEntrada = "tabuleiro.txt";
+	private static final String nomeJogadas = "jogadas.txt";
+	private static final String nomeSaida = "estatistica.txt";
+	
+	private static PosicaoTabuleiro[] tabuleiro;
+	private static int numPosicoes;
 	
 	private static int numJogadores;
 	private static double saldoInicialJogadores;
@@ -11,9 +19,59 @@ public class BancoImobiliario {
 	
 	private static int rodadas;
 	
-	//TODO
+	/**
+	 * Constroi o tabuleiro de jogo a partir do arquivo de entrada.
+	 */
 	private static void constroiTabuleiro() {
-		
+		try {
+			File entrada = new File(nomeEntrada);
+			Scanner leitor = new Scanner(entrada);
+			
+			String[] linha; // Corresponde a uma linha do arquivo tabuleiros.txt
+			int posicao; // Posição a ser inserida no tabuleiro.
+			int tipoPosicao; // Tipo da posição a ser inserida no tabuleiro.
+			int tipoImovel; // Tipo do imóvel, caso a posição represente um.
+			double valorImovel; // Valor do imóvel, caso a posição represente um.
+			double taxaAluguel; // Aluguel, caso a posição represente um imóvel. 
+			
+			numPosicoes = Integer.parseInt(leitor.nextLine());
+			
+			tabuleiro = new PosicaoTabuleiro[numPosicoes];
+			
+			for(int i = 0; i < numPosicoes; i++) {
+				linha = leitor.nextLine().split(";");
+				
+				posicao = Integer.parseInt(linha[1]);
+				tipoPosicao = Integer.parseInt(linha[2]);
+				
+				PosicaoTabuleiro novaPosicao = null;
+				
+				/* Analisa o tipo da posição */
+				switch(tipoPosicao) {
+				case PosicaoTabuleiro.INICIO:
+				case PosicaoTabuleiro.PASSA:
+					novaPosicao = new PosicaoTabuleiro(tipoPosicao, null);
+					break;
+					
+				case PosicaoTabuleiro.IMOVEL:
+					tipoImovel = Integer.parseInt(linha[3]);
+					valorImovel = Double.parseDouble(linha[4]);
+					taxaAluguel = Double.parseDouble(linha[5]);
+					
+					Imovel novoImovel = new Imovel(tipoImovel, valorImovel,
+							taxaAluguel);
+					
+					novaPosicao = new PosicaoTabuleiro(tipoPosicao, novoImovel);
+					break;
+				}
+				
+				tabuleiro[posicao - 1] = novaPosicao;
+			}
+			
+			leitor.close();
+		} catch(FileNotFoundException fnfe) {
+			System.out.println("Erro: arquivo \"tabuleiros.txt\" não encontrado.");
+		}
 	}
 	
 	//TODO
--- src/jogo/PosicaoTabuleiro.java
+++ src/jogo/PosicaoTabuleiro.java
@@ -1,12 +1,16 @@
 package jogo;
 
 public class PosicaoTabuleiro {
-	/**
-	 * Tipo das posições:
-	 * Iniciar -> 1
-	 * Passe a vez -> 2
-	 * Imóvel -> 3
-	 */
+	/* Tipo das posições */
+	public static final int INICIO = 1;
+	public static final int PASSA = 2;
+	public static final int IMOVEL = 3;
+	
 	private int tipo;
 	private Imovel imovel;
+	
+	public PosicaoTabuleiro(int tipo, Imovel imovel) {
+		this.tipo = tipo;
+		this.imovel = imovel;
+	}
 }
